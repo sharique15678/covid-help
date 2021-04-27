88febd63f68ad019c455084a54d83828b98c229f
Md Sharique <sharique15678@gmail.com>
2021-04-27 08:53:18 +0530

    just finalising

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session, make_response
+from flask import Flask, render_template, request
 from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
@@ -286,4 +286,4 @@ def give_help():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
